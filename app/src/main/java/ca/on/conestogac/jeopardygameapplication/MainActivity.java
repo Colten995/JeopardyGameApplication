7942431e18492cb9c0ebf9257bf5e199b7119b62
abdyh <aali3585@conestogac.on.ca>
2021-03-29 21:26:24 -0400

    Added drawer layout.

--- app/src/main/java/ca/on/conestogac/jeopardygameapplication/MainActivity.java
+++ app/src/main/java/ca/on/conestogac/jeopardygameapplication/MainActivity.java
@@ -1,95 +1,109 @@
 package ca.on.conestogac.jeopardygameapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonPoints200;
-    private Button buttonPoints400;
-    private Button buttonPoints600;
-    private Button buttonPoints800;
-    private Button buttonPoints1000;
 
-    private Button buttonNewGame;
-    private Button buttonDailyDouble;
-
-    private TextView textViewScore;
-    private Button buttonSecondRound;
-    private Button buttonBackToFirstRound;
-    private Button buttonFinalJeopardy;
-
-    private int totalScore;
-    private final int DEFAULT_SCORE = 0;
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        View.OnClickListener pointsListener = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                switch (v.getId()){
-                    case R.id.buttonPoints200:
-                        totalScore += 200;
-                        break;
-                    case R.id.buttonPoints400:
-                        totalScore += 400;
-                        break;
-                    case R.id.buttonPoints600:
-                        totalScore += 200;
-                        break;
-                    case R.id.buttonPoints800:
-                        totalScore += 200;
-                        break;
-                    case R.id.buttonPoints1000:
-                        totalScore += 200;
-                        break;
-                    default:
-                        break;
-                }
-
-                textViewScore.setText(String.valueOf(totalScore));
-            }
-        };
-
-        View.OnClickListener newGameListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                totalScore = DEFAULT_SCORE;
-                textViewScore.setText(String.valueOf(totalScore));
-            }
-        };
 
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //instantiate our drawer layout ui reference and make a new toggle button for our navigation drawer menu
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        //add our toggle to the activity main drawer layout xml
+        drawerLayout.addDrawerListener(toggle);
+        //sync the state of open or close to our toggle
+        toggle.syncState();
+        //we now enable our toggle
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //user defined method is called to open the fragments based on the option chose in our navigation menu
+        setupNavDrawer();
+        Fragment fragment = new ScoreFragment();
+        //open a fragment transaction which we will use to replace our main activity layout with the fragment layout
+        FragmentTransaction fragTransaction = getSupportFragmentManager().beginTransaction();
+        //replace our framelayout in activity_main xml with the fragment
+        fragTransaction.replace(R.id.frame_layout, fragment);
+        fragTransaction.commit();
+        //then close the drawer menu once an item has been selected
+        drawerLayout.closeDrawers();
 
-        buttonPoints200 =findViewById(R.id.buttonPoints200);
-        buttonPoints400 = findViewById(R.id.buttonPoints400);
-        buttonPoints600 = findViewById(R.id.buttonPoints600);
-        buttonPoints800 = findViewById(R.id.buttonPoints800);
-        buttonPoints1000 = findViewById(R.id.buttonPoints1000);
 
-        buttonNewGame = findViewById(R.id.buttonNewGame);
-        buttonDailyDouble = findViewById(R.id.buttonDailyDouble);
+    }
+    private void setupNavDrawer() {
+        //instantiate our navigation menu ui reference
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        //set a navigation listener to know which fragment is selected
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //declare our fragment and put it to null for now until a menu option is selected
+                Fragment frag = null;
+                //going into our res > menu folder with our menu layout (drawer_menu.xml) i grab the id of the option selected so i know which one is selected
+                int itemID = item.getItemId();
+                //make an if, else if statement to open the fragment based on option selected
+                //this first if statement stores my first fragment tic tac toe into Fragment frag if it is selected
+                if(itemID == R.id.nav_points){
+                    frag = new PointsFragment();
+                }
+                //if tic tac toe fragment isnt selected we need to check if our second option (view records) option is selected
+                //store a new instance of the records fragment into our frag variable
+                else if (itemID == R.id.nav_score){
+                    frag = new ScoreFragment();
+                }
+                //lastly if tic tac toe and view records item was not selected, we check if user selects the contacts list open then store a new instance of the fragment into my frag variable to be displayed later
+                else if (itemID == R.id.nav_new_game){
+                    frag = new NewGameFragment();
+                }
+                else if (itemID == R.id.nav_settings){
+                    frag = new NewGameFragment();
+                }
+                //now we check if a fragment not empty, meaning theres an instance of a fragment
+                //so we load that into our activity_main.xml and open it, then close the drawers of our menu
+                if(frag != null){
+                    //open a fragment transaction which we will use to replace our main activity layout with the fragment layout
+                    FragmentTransaction fragTransaction = getSupportFragmentManager().beginTransaction();
+                    //replace our framelayout in activity_main xml with the fragment
+                    fragTransaction.replace(R.id.frame_layout, frag);
+                    //commit the transaction
+                    fragTransaction.commit();
+                    //then close the drawer menu once an item has been selected
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
 
-        textViewScore = findViewById(R.id.textViewScore);
-        buttonBackToFirstRound =findViewById(R.id.buttonFirstRound);
-        buttonSecondRound = findViewById(R.id.buttonSecondRound);
-        buttonFinalJeopardy = findViewById(R.id.buttonFinalRound);
+                //if frag is empty, then return false because there still hasn't been an option selected.
+                return false;
+            }
+        });
+    }
 
-        buttonPoints200.setOnClickListener(pointsListener);
-        buttonPoints400.setOnClickListener(pointsListener);
-        buttonPoints600.setOnClickListener(pointsListener);
-        buttonPoints800.setOnClickListener(pointsListener);
-        buttonPoints1000.setOnClickListener(pointsListener);
-
-        buttonNewGame.setOnClickListener(newGameListener);
+    //by default a menu item is set to false because it has not been touched and so this method is if user selects an item from the menu, it will return true because an item has been selected
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
