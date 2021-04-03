7c143a0718549ef89967e21f4a61a3f872f7d6b1
Colten995 <Cgoetz9919@conestogac.on.ca>
2021-04-02 23:03:13 -0400

    Created the points dialog box layout. TODO: Change colors of background, buttons and style text

---
+++ app/src/main/java/ca/on/conestogac/jeopardygameapplication/PointsDialog.java
@@ -0,0 +1,14 @@
+package ca.on.conestogac.jeopardygameapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PointsDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_points_dialog);
+    }
+}
