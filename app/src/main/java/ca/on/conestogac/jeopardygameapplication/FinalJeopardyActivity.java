0c92e43e5c0d43743c1adaf05f0b0fd7550338fd
Colten995 <Cgoetz9919@conestogac.on.ca>
2021-04-06 17:28:00 -0400

    Created activity_final_jeopardy.xml and started designing the layout.

---
+++ app/src/main/java/ca/on/conestogac/jeopardygameapplication/FinalJeopardyActivity.java
@@ -0,0 +1,14 @@
+package ca.on.conestogac.jeopardygameapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalJeopardyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_jeopardy);
+    }
+}
