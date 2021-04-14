365b942ee5ca8f50ded3caaa9264462d97c3540c
Colten995 <Cgoetz9919@conestogac.on.ca>
2021-04-14 19:39:52 -0400

    Started dynamically creating the table for the high score table, modified some of the getHighScore method. Need to figure out why the textViews are not displaying in the high score table

--- app/src/main/java/ca/on/conestogac/jeopardygameapplication/HighScoreActivity.java
+++ app/src/main/java/ca/on/conestogac/jeopardygameapplication/HighScoreActivity.java
@@ -10,8 +10,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class HighScoreActivity extends AppCompatActivity {
 
@@ -21,26 +26,31 @@ public class HighScoreActivity extends AppCompatActivity {
     TextView name;
     TextView score;
 
+    TableLayout highScoreTableLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
         //back = findViewById(R.id.btnBack);
-        name = findViewById(R.id.txtName);
+        /*name = findViewById(R.id.txtName);
         score = findViewById(R.id.txtScore);
-        /*back.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
-        });*/
+        });
         scoreDatabaseHelper = new ScoreDatabaseHelper(this);
-//        ShowScores();
+//        ShowScores();*/
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        highScoreTableLayout = findViewById(R.id.tableLayoutHighScores);
+        refreshHighScores();
     }
 
     @Override
@@ -86,7 +96,37 @@ public class HighScoreActivity extends AppCompatActivity {
     }
     private void refreshHighScores() {
         //Display all high scores in the table
-        int highScores = ((JeopardyApplication)getApplication()).getHighScores();
-        textViewHighScores.setText(String.valueOf(highScores));
+        Cursor highScoreCursor = ((JeopardyApplication)getApplication()).getHighScores();
+        int numberOfTableRows = highScoreCursor.getCount();
+
+        TextView textViewUsername = new TextView(this);
+        TextView textViewScore = new TextView(this);
+        LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+        );
+        /*TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        );*/
+        textViewUsername.setLayoutParams(textViewParams);
+        textViewScore.setLayoutParams(textViewParams);
+
+        highScoreCursor.moveToFirst();
+        for(int i = 0; i < numberOfTableRows; i++)
+        {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableRow.LayoutParams());
+            textViewUsername.setText(highScoreCursor.getString(1));
+            textViewScore.setText(String.valueOf(highScoreCursor.getInt(2)));
+            tableRow.addView(textViewUsername);
+            tableRow.addView(textViewScore);
+            highScoreTableLayout.addView(tableRow);
+            highScoreCursor.moveToNext();
+        }
+
+        highScoreCursor.close();
+
     }
 }
